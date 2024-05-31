c828ea72ed6879e3e6e5c13cea40a49a2bebe5c9
Nikho311 <nikho311.dev@gmail.com>
2024-05-31 23:37:32 +0200

    add: parallax effect done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import './app.scss';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
+import Parallax from './components/parallax/Parallax';
 const App = () => {
   return <div>
     <section id="acceuil">
       <Navbar/>
       <Hero/>
     </section>
-    <section id="competences">Parallax</section>
+    <section id="competences">
+      <Parallax type="skills"/>
+    </section>
+
     <section>Compétences + CV</section>
-    <section id="projets">Parallax</section>
+    
+    <section id="projets">
+      <Parallax type="projects"/>
+    </section>
+    
     <section>Projects</section>
+    
     <section id="contact">Contact</section>
   </div>;
 };
